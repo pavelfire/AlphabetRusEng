4e05a88a3c9471c3dedfc9561ff7537c6c23a16c
PavelWrki5W10 <fire_ra@bk.ru>
2021-11-16 17:12:23 +0300

    about screen edited

--- app/src/main/java/com/vk/directop/alphabetruseng/LetterAdapter.kt
+++ app/src/main/java/com/vk/directop/alphabetruseng/LetterAdapter.kt
@@ -1,11 +1,13 @@
 package com.vk.directop.alphabetruseng
 
+import android.graphics.Color
 import android.media.MediaPlayer
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -49,8 +51,14 @@ class LetterAdapter(letterViews: List<Letter>) :
         val currentView = list[position]
         val ivImage: ImageView = holder.itemView.findViewById(R.id.iv_image)
         val tvDescription: TextView = holder.itemView.findViewById(R.id.tv_description)
+        val relLayItem : RelativeLayout = holder.itemView.findViewById(R.id.rel_lay_item)
 
         holder.bind(currentView)
+
+        val colorsBackground = listOf("#B2D3B2", "#D3B2B2", "#D3BEB2", "#D2D3B2", "#C6D3B2",
+            "#B2D3CF", "#B2D3B2", "#B2B5D3", "#CEB2D3", "#B7B2D3", "#D3B2B6")
+
+        relLayItem.setBackgroundColor(Color.parseColor(colorsBackground.random().toString()))
 
 
         ivImage.setImageResource(currentView.imageId)
--- app/src/main/java/com/vk/directop/alphabetruseng/MainActivity.kt
+++ app/src/main/java/com/vk/directop/alphabetruseng/MainActivity.kt
@@ -3,6 +3,9 @@ package com.vk.directop.alphabetruseng
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Switch
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
@@ -11,24 +14,44 @@ private const val LAST_SELECTED_MENU = "LAST_SELECTED_MENU"
 class MainActivity : AppCompatActivity() {
 
     private lateinit var bottomMenu : BottomNavigationView
+    private lateinit var switch : Switch
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        switch = findViewById(R.id.switch1)
         bottomMenu = findViewById(R.id.bottom_nav)
+
+//        if(switch.isActivated){
+//            Toast.makeText(this, "Switch on", Toast.LENGTH_SHORT).show()
+//        }else {
+//            Toast.makeText(this, "Switch off", Toast.LENGTH_SHORT).show()
+//        }
+        switch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                Toast.makeText(this, "Switch on", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(this, "Switch off", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
 
         bottomMenu.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.rus_alphabet -> {
                     val menuFragment = RusFragment()
                     replaceFragment(menuFragment)
+                    switch.visibility = View.VISIBLE
                 }
                 R.id.eng_alphabet -> {
+
                     var mediaPlayer = MediaPlayer.create(applicationContext, R.raw.c)
                     mediaPlayer.start()
 
                     val favoriteFragment = ListLettersFragment()
                     replaceFragment(favoriteFragment)
+                    switch.visibility = View.VISIBLE
                 }
                 R.id.about -> {
 
@@ -37,6 +60,7 @@ class MainActivity : AppCompatActivity() {
 
                     val aboutFragment = AboutFragment()
                     replaceFragment(aboutFragment)
+                    switch.visibility = View.GONE
                 }
             }
             true
